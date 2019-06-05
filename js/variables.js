30deeda85003cccedf77d1438dbbd008cc0ccaa2
Jan Prchal <prchal.jan@outlook.com>
2019-06-05 14:30:40 +0100

    starting to do the main and desktop

---
+++ js/variables.js
@@ -0,0 +1,13 @@
+// Just for development purposes
+window.addEventListener('click', function(e) {
+  console.log(e.target);
+})
+
+
+buttons = {
+  openMenu: document.querySelector('.header__menu-icon'),
+  closeMenu: document.querySelector('.nav__menu-icon'),
+
+}
+
+console.log(buttons.openMenu);
