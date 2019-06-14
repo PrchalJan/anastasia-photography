6818fa8330f7ea862f43807d3f53f6bc0dd9a206
Jan Prchal <prchal.jan@outlook.com>
2019-06-14 15:58:27 +0100

    home resizing

--- js/resize.js
+++ js/resize.js
@@ -21,12 +21,14 @@ window.addEventListener('resize', debounce(function() {
   console.log(widthBank);
   if(!(widthBank === window.innerWidth)) {
     setProperHomeHeight();
+    
+    if(sections.home.style.display === 'block') {
+      showHomeSection();
+    }
   }
   widthBank = window.innerWidth;
   console.log(sections.home.style.display);
-  if(sections.home.style.display === 'block') {
-    showHomeSection();
-  }
+
   
 
 }, 350));
