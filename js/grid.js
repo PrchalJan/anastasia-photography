2a01082c868f11d2177aebca25d2334d644fb1e3
Jan Prchal <prchal.jan@outlook.com>
2019-06-17 21:24:52 +0100

    grid polished

--- js/grid.js
+++ js/grid.js
@@ -1,82 +1,31 @@
 export const grid = function() {
 
-// function renderGrid(grids, colCount, padding){
-//   var pad = padding || 0;
-//   var newleft, newtop;
-//   for(var i = 1; i < grids.length; i++){
-//     if (i % colCount == 0) {
-//       newtop = (grids[i-colCount].offsetTop + grids[i-colCount].offsetHeight) + pad;
-//         grids[i].style.top = newtop+"px";
-//     } else {
-//       if(grids[i-colCount]){
-//         newtop = (grids[i-colCount].offsetTop + grids[i-colCount].offsetHeight) + pad;
-//         grids[i].style.top = newtop+"px";
-//       }
-//       newleft = (grids[i-1].offsetLeft + grids[i-1].offsetWidth) + pad;
-//       grids[i].style.left = newleft+"px";	
-//     }
-//     }
-// }
-
-// function renderArchiveGrid(colCount, padding) {
-//   const grids = document.querySelectorAll('.archive__figure');
-//   return renderGrid.call(this, grids, colCount, padding)
-// }
-
-// window.document.body.addEventListener('load', function() {
-//   renderArchiveGrid(3);
-// }, false);
-
-// from scratch
-
-function setGridBlockElementsWidth(grids, colCount) {
+function setGridsWidth(grids, colCount) {
   for(let i = 0; i < grids.length; i++) {
     grids[i].style.width = ((100 / colCount) + '%');
-    console.log(grids[i]);
+    // console.log(grids[i]);
   }
 }
-
-function renderGrid(columns){
-  var colCount = columns || 1;
-  var grids = document.querySelectorAll('.archive__figure');
-  var container = grids[0].parentElement;
-  setGridBlockElementsWidth(grids, colCount)
-
-
-	// var newleft, newtop;
-	// for(var i = 1; i < grids.length; i++){
-	// 	if (i % colCount == 0) {
-	// 		newtop = (grids[i-colCount].offsetTop + grids[i-colCount].offsetHeight);
-	// 	    grids[i].style.top = newtop+"px";
-	// 	} else {
-	// 		if(grids[i-colCount]){
-	// 			newtop = (grids[i-colCount].offsetTop + grids[i-colCount].offsetHeight);
-	// 			grids[i].style.top = newtop+"px";
-	// 		}
-	// 		newleft = (grids[i-1].offsetLeft + grids[i-1].offsetWidth);
-	// 		grids[i].style.left = newleft+"px";	
-	// 	}
-  // }
-  function placeElements(grids,colCount) {
-    let newleft, newtop;
-    for(let i = 1; i < grids.length; i++){
-      if (i % colCount == 0) {
+function placeGrids(grids,colCount) {
+  let newleft, newtop;
+  for(let i = 1; i < grids.length; i++){
+    if (i % colCount == 0) {
+      newtop = (grids[i-colCount].offsetTop + grids[i-colCount].offsetHeight);
+        grids[i].style.top = newtop + "px";
+    } else {
+      if(grids[i-colCount]){
         newtop = (grids[i-colCount].offsetTop + grids[i-colCount].offsetHeight);
-          grids[i].style.top = newtop + "px";
-      } else {
-        if(grids[i-colCount]){
-          newtop = (grids[i-colCount].offsetTop + grids[i-colCount].offsetHeight);
-          grids[i].style.top = newtop + "px";
-        }
-        newleft = (grids[i-1].offsetLeft + grids[i-1].offsetWidth);
-        grids[i].style.left = newleft + "px";	
+        grids[i].style.top = newtop + "px";
       }
+      newleft = (grids[i-1].offsetLeft + grids[i-1].offsetWidth);
+      grids[i].style.left = newleft + "px";	
     }
   }
-  placeElements(grids, colCount);
-  
+}
+function setGridContainerHeight(grids, colCount) {
+  var container = grids[0].parentElement;
   var containerHeight = 0;
-  for(var i = ((grids.length - colCount)-1); i < grids.length -1; i++) {
+  for(var i = ((grids.length - colCount)-1); i < grids.length; i++) {
     let topPosition = grids[i].offsetTop;
     let height = parseInt(getComputedStyle(grids[i]).height);
     let newContainerHeight = topPosition + height;
@@ -86,12 +35,16 @@ function renderGrid(columns){
       containerHeight = newContainerHeight;
     }
   }
-
-  var lastBlockTopPosition = grids[grids.length -1].offsetTop;
-  var lastBlockHeight = parseInt(getComputedStyle(grids[grids.length -1]).height);
-  console.log(lastBlockTopPosition + lastBlockHeight);
   container.style.height = (containerHeight) + 'px';
+}
 
+function renderGrid(columns){
+  var colCount = columns || 1;
+  var grids = document.querySelectorAll('.archive__figure');
+
+  setGridsWidth(grids, colCount)
+  placeGrids(grids, colCount);
+  setGridContainerHeight(grids, colCount);
 }
 window.addEventListener("load", function() {
   renderGrid(3);
