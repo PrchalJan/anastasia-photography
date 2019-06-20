244121d4926f80b5d47797bcd9de7e55f1e70f4e
Jan Prchal <prchal.jan@outlook.com>
2019-06-20 16:52:53 +0100

    now the lightbox open proper image

--- js/lightbox.js
+++ js/lightbox.js
@@ -1,15 +1,23 @@
 export const lightbox = function() {
 
+
+function closeLightbox() {
+  const currentImage = document.querySelector('#lightbox__image');
+  document.body.classList.remove('lightbox-open');
+  states.lightbox = false;
+  currentImage.src = '';
+
+}
+
 window.closeLightboxBtnClick = function() {
   if(states.lightbox) {
-    document.body.classList.remove('lightbox-open');
-    states.lightbox = false;
+
+    closeLightbox();
   }
 }
 window.closeLightboxGapClick = function(e) {
   if(states.lightbox && e.target === components.lightbox) {
-    document.body.classList.remove('lightbox-open');
-    states.lightbox = false;
+    closeLightbox();
   }
 }
 
@@ -20,10 +28,16 @@ window.openLightbox = function() {
   }
 }
 
-window.setLightboxImage = function(src) {
+
+window.setLightboxImages = function(imgSet, index, quality) {
   const lightboxImage = document.querySelector('#lightbox__image');
+  const image = imgSet[index];
+  const newSrc = image.getAttribute('data-src').replace('?', quality);
+  const src = newSrc;
   lightboxImage.src = src;
 }
+
+// setLightboxImages(imgs.archive, 0, 1920);
 
 
 // window.addEventListener('click', function() {
@@ -38,9 +52,21 @@ window.setLightboxImage = function(src) {
 //   }
 // });
 
+function getClickedImageIndex(imgArr, img) {
+  for(let i = 0; i < imgArr.length; i++) {
+    if(imgArr[i] === img) {
+      return i
+    }
+  }
+}
+
 window.openArchiveLightboxClick = function(e) {
-  if(e.target.firstElementChild.classList.contains('archive__img')) {
+  const imgs = document.querySelectorAll('.archive__img');
+  const img = e.target.firstElementChild;
+  if(img.classList.contains('archive__img')) {
+    const imgIndex = getClickedImageIndex(imgs, img);
     openLightbox();
+    setLightboxImages(imgs, imgIndex, 1280)
   }
 }
 
